4133c4219e32215d7cdfbbefa6356a98a501744b
이지원 <uk9336@naver.com>
2022-05-27 23:52:29 +0900

    [add] 동네생활 페이크 데이터 추가

---
+++ app/src/main/java/com/jw/cloneappcarrot/model/NeighborModel.kt
@@ -0,0 +1,20 @@
+package com.jw.cloneappcarrot.model
+
+/**
+ * Created by LJW on 2022/05/27.
+ *
+ * Description :
+ */
+data class NeighborModel(
+    val id: Int,
+    val type: String,
+    val nickName: String,
+    val address: String,
+    val content: String,
+    val time: String,
+    val imageUrl1: String,
+    val imageUrl2: String,
+    val imageUrl3: String,
+    val good: Int,
+    val reply: Int,
+)
